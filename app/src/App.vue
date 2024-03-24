a3a2422025fb954a1880b11ec0502d000dc5f729
Vladimir Pilipenko <pilip2004@gmail.com>
2024-03-24 17:32:20 +0300

    HomeWork_07

---
+++ app/src/App.vue
@@ -0,0 +1,42 @@
+
+<template>
+  <div id="app">
+    <HeaderComp />
+    <ProjectDetailsComp />
+    <FooterComp />
+  </div>
+</template>
+
+<script>
+
+import BlogComp from './components/BlogComp.vue'
+import HeaderComp from './components/HeaderComp.vue';
+import FooterComp from './components/FooterComp.vue';
+import BlogDetailsComp from './components/blogDetailsComp.vue';
+import ProjectComp from './components/ProjectComp.vue';
+import ProjectDetailsComp from './components/ProjectDetailsComp.vue';
+
+export default {
+  name: 'App',
+  components: {
+
+    BlogComp,
+    BlogDetailsComp,
+    HeaderComp,
+    FooterComp,
+    ProjectComp,
+    ProjectDetailsComp
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
